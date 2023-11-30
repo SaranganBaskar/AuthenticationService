44fd772c8039a60124abe9e416b3e09f96e8e22b
Sarangan Baskar <saranganb1993@gmail.com>
2023-11-30 16:09:03 +0530

    OAuth Implementation with Custom Models Implemented

--- src/main/java/dev/sarangan/authenticationservice/security/models/CustomUserDetails.java
+++ src/main/java/dev/sarangan/authenticationservice/security/models/CustomUserDetails.java
@@ -1,8 +1,10 @@
 package dev.sarangan.authenticationservice.security.models;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import dev.sarangan.authenticationservice.models.Role;
 import dev.sarangan.authenticationservice.models.User;
 import dev.sarangan.authenticationservice.security.services.CustomUserDetailsService;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -10,48 +12,72 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+@JsonDeserialize
+@NoArgsConstructor
 public class CustomUserDetails implements UserDetails {
-    private User user;
+    //    private User user;
+    private String username;
+    private String password;
+    private List<GrantedAuthority> authorities;
+    private boolean isAccountNonExpired;
+    private boolean isAccountNonLocked;
+    private boolean isCredentialsNonExpired;
+    private boolean isEnabled;
 
-    public CustomUserDetails(User user){
-        this.user = user;
+
+    public CustomUserDetails(User user) {
+        authorities = new ArrayList<>();
+        for (Role role : user.getRoles()) {
+            authorities.add(new CustomGrantedAuthority(role));
+        }
+
+        this.password = user.getPassword();
+        this.username = user.getEmail();
+        this.isAccountNonExpired = true;
+        this.isAccountNonLocked = true;
+        this.isCredentialsNonExpired = true;
+        this.isEnabled = true;
     }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        List<CustomGrantedAuthority> customGrantedAuthorities = new ArrayList<>();
+        /*List<CustomGrantedAuthority> customGrantedAuthorities = new ArrayList<>();
         for(Role role: user.getRoles()){
             customGrantedAuthorities.add(new CustomGrantedAuthority(role));
-        }
-        return customGrantedAuthorities;
+        }*/
+        return this.authorities;
     }
 
     @Override
     public String getPassword() {
-        return user.getPassword();
+        return this.password;
+//        return user.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return user.getEmail();
+        return this.username;
+//        return user.getEmail();
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return true;
+
+        return isAccountNonExpired;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return true;
+        return isAccountNonLocked;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return true;
+        return isCredentialsNonExpired;
     }
 
     @Override
     public boolean isEnabled() {
-        return true;
+        return isEnabled;
     }
 }
